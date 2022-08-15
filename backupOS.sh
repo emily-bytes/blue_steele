76e48d00d351abf9e2d47751b76116ff22e24989
emily <emilit@emilit>
2022-08-15 22:55:39 +0000

    addded backup OS file

---
+++ backupOS.sh
@@ -0,0 +1,10 @@
+# Script: Blue Steel Setup
+# Author: Emily Lee
+# Date: August 15, 2022
+# Purpose: The entire OS is backed up to the 
+# backup drive 
+
+# command that copies OS from Windows to backup
+# drive D
+ROBOCOPY.EXE "C:/Windows" "E:\UserDataBackup"
+
